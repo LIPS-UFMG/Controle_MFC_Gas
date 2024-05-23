f10bc6b6ce4990d998eb6abb0ddb6dcdba540b5e
lucasbmedeiros2021 <lucasbmedeiros2021@gmail.com>
2024-05-23 17:13:42 -0300

    Add files via upload

--- Display_MFC2_Gas.ino
+++ Display_MFC2_Gas.ino
@@ -1,39 +1,18 @@
 /***************************
-* Teclado Matricial 16 Teclas : Primeiros Passos (v1.0)
-*
-* Codigo base para exibir as teclas pressionadas no monitor serial da IDE.
-*
-* Copyright 2020 RoboCore.
-* Escrito por Matheus Cassioli (30/07/2019).
-* Atualizado por Giovanni de Castro (22/01/2020).
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version (<https://www.gnu.org/licenses/>).
+
 ***************************/
 
 #include <Keypad.h>         // Biblioteca do codigo
-#include <LiquidCrystal.h>  //Biblioteca do display
+#include <LiquidCrystal.h>  // Biblioteca do display
 
 const byte LINHAS = 4;   // Linhas do teclado
 const byte COLUNAS = 4;  // Colunas do teclado
 
-const int mainMenu = 1;
-const int selectMFC = 2;
-const int insertData = 3;
-const char backMarker = 'B';
-const char endMarker = 'D';
-int event = 1;
+//-----------------------------------------PINAGEM----------------------------------------//
 
-byte messageIndex = 0;
-const byte buffSize = 20;
-char inputBuffer2[buffSize];  // Buffer para a entrada do display
-
-char messageFromPC[buffSize] = { 0 };
-int intFromPC = 0;
-float floatFromPC = 0.0;  // fraction of servo range to move
-
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);             // Pinos do lcd
+byte PINOS_LINHAS[LINHAS] = { A0, A1, 7, 6 };      // Pinos de conexao com as linhas do teclado
+byte PINOS_COLUNAS[COLUNAS] = { A2, A3, A4, A5 };  // Pinos de conexao com as colunas do teclado
 
 const char TECLAS_MATRIZ[LINHAS][COLUNAS] = {  // Matriz de caracteres (mapeamento do teclado)
   { '1', '2', '3', 'A' },
@@ -42,105 +21,94 @@ const char TECLAS_MATRIZ[LINHAS][COLUNAS] = {  // Matriz de caracteres (mapeamen
   { '*', '0', '#', 'D' }
 };
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-byte PINOS_LINHAS[LINHAS] = { A0, A1, 7, 6 };      // Pinos de conexao com as linhas do teclado
-byte PINOS_COLUNAS[COLUNAS] = { A2, A3, A4, A5 };  // Pinos de conexao com as colunas do teclado
-
-int a = 0;
-int b = 0;
-
 Keypad teclado_personalizado =
   Keypad(makeKeymap(TECLAS_MATRIZ), PINOS_LINHAS, PINOS_COLUNAS, LINHAS, COLUNAS);  // Inicia teclado
 
+//----------------------------------------VARIÁVEIS---------------------------------------//
+
+int event = 1;                                                                  // Indice para telas de interação, comecando na primeira
+const int mainMenu = 1, selectMFC = 2, insertData = 3;                          // Telas de interação
+const char comma = '#', backMarker = 'B', clearMarker = 'C', doneMarker = 'D';  // Teclas especiais
+
+const byte buffSize = 20;     // Tamanho máximo de inserçao
+char inputBuffer2[buffSize];  // Buffer para a entrada do display
+byte messageIndex = 0;        // Indice para manipular array
+
+char messageFromPC[buffSize] = { 0 };  // Variável para configuracao(?)
+int intFromPC = 0;                     // MFC selecionada
+float floatFromPC = 0.0;               // fraction of servo range to move(?)
+
 void setup() {
   Serial.begin(9600);  // Inicia porta serial
-  lcd.begin(16, 2);
+  lcd.begin(16, 2);    // Inicializa lcd
 }
 
 void loop() {
-  getDataFromDisplay();
-  delay(30);
+  printToLcd();           // Impressão de instruções e valores inseridos
+  getDataFromKeyboard();  // Pega informações do teclado
+  delay(50);
 }
 
 
-void getDataFromDisplay() {
-
+void getDataFromKeyboard() {
   lcd.setCursor(0, 0);
-  if (event == mainMenu) {  //imprime mensagem na tela
-    lcd.print("Escolha: 1 2 3 4");
-  } else if (event == selectMFC) {
-    lcd.println("1-MFC1 2-MFC2");
-  } else if (event == insertData) {
-    lcd.println("Digite um valor:");
-  }
-  lcd.setCursor(0, 1);
-  lcd.print(inputBuffer2);
-
   char leitura_teclas = teclado_personalizado.getKey();  // Atribui a variavel a leitura do teclado
 
-  if (leitura_teclas == endMarker) {  // se dado foi inserido
-    bool validData = false;           // variavel para indicar se dado inserido é valido
-    Serial.println("Mensagem recebida:");
-    Serial.println(inputBuffer2);
-    messageIndex = 0;
-    int input = atof(inputBuffer2);  // converte entrada para numero para comparar
-
-    if (event == mainMenu) {  // confere parametros
-      if (input == 1 || input == 2 || input == 3 || input == 4) {
-        validData = true;
-      }
-    } else if (event == selectMFC) {
-      if (input == 1 || input == 2) {
-        validData = true;
-      }
-
-    } else if (event == insertData) {
-      validData = true;
-    }
-
-    if (validData) {  // se dado for válido chama parse 2 e passa pro proximo evento
-      parseData2();
-      if (event == 3) {
-        event = 0;
-      }
-      event++;
-
-    } else {  // se dado invalido limpa buffer
-
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("Numero invalido");
-      delay(1000);
-    }
-    lcd.clear();
-
-    for (int i = 0; i < buffSize; i++) {  //limpa buffer
-      inputBuffer2[i] = '\0';             // Preenche o buffer com caracteres nulos
-    }
-
-  } else if (leitura_teclas != NO_KEY) {  //processo para ler teclas
-    if (messageIndex < buffSize) {        //caso mensagem seja menor que limite do buffer
-      //Serial.print(leitura_teclas);
+  if (leitura_teclas != NO_KEY) {   // Caso tecla não nula
+    if (messageIndex < buffSize) {  // Impede que a mensagem ultrapasse o limite
       switch (leitura_teclas) {
-        case '#':
+        case (comma):  // Tecla #, substituida por ponto
           inputBuffer2[messageIndex] = '.';
           messageIndex++;
           break;
-        case 'B':
+
+        case (backMarker):  // Tecla B, volta para tela anterior
           for (int i = 0; i < buffSize; i++) {
             inputBuffer2[i] = '\0';  // Preenche o buffer com caracteres nulos
           }
           messageIndex = 0;
           event--;
           break;
-        case 'C':
-          messageIndex--;
-          inputBuffer2[messageIndex] = '\0';
+
+        case (clearMarker):                                // Tecla C, apaga último caractere
+          if (messageIndex != "\0" && messageIndex > 0) {  // Caso array não nulo
+            messageIndex--;
+            inputBuffer2[messageIndex] = '\0';
+          }
+          break;
+
+        case (doneMarker):  // Tecla D, submete valor inserido
+          Serial.println("Mensagem recebida:");
+          Serial.println(inputBuffer2);
+
+          messageIndex = 0;                // Zera indice do array
+          int input = atoi(inputBuffer2);  // Converte entrada em string para double
+          Serial.println("input");
+          Serial.println(input);
+          if (validData(input)) {  // Se dado inserido for válido
+            parseData2();          // Envia dados
+            if (event == 3) {      // Caso tenha terminado o processo
+              event = 0;
+              lcd.clear();
+              lcd.print("Confirmado");
+              delay(1000);
+            }
+            event++;  // Passa para próxima tela
+
+          } else {  // Se dado invalido
+            lcd.clear();
+            lcd.setCursor(0, 1);
+            lcd.print("Numero invalido");
+            delay(1000);
+          }
+          lcd.clear();
+
+          for (int i = 0; i < buffSize; i++) {  // Limpa buffer
+            inputBuffer2[i] = '\0';             // Preenche o buffer com caracteres nulos
+          }
           break;
         default:
           inputBuffer2[messageIndex++] = leitura_teclas;
-          break;
       }
       lcd.clear();
     } else {
@@ -149,6 +117,43 @@ void getDataFromDisplay() {
     }
   }
 }
+
+void printToLcd() {
+  lcd.setCursor(0, 1);
+  lcd.print(inputBuffer2);
+  lcd.setCursor(0, 0);
+  switch (event) {  // Printa mensagem correspondente a tela
+    case mainMenu:
+      lcd.print("Escolha: 1 2 3 4");
+      break;
+    case selectMFC:
+      lcd.println("1-MFC1 2-MFC2");
+      break;
+    case insertData:
+      lcd.println("Digite um valor:");
+      break;
+  }
+}
+
+bool validData(int input) {  // Realiza validação dos dados para cada tela
+
+  bool ret = false;
+
+  switch (event) {
+    case mainMenu:
+      ret = (input == 1 || input == 2 || input == 3 || input == 4);
+      break;
+    case selectMFC:
+      ret = (input == 1 || input == 2);
+      break;
+    case insertData:
+      ret = true;
+      break;
+  }
+
+  return ret;
+}
+
 
 void parseData2() {
   delay(50);
